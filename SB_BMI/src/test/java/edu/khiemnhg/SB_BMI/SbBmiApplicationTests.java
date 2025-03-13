1053ebfb81444e37cdee8cb85fbb8fcd27786151
KhiemCho <khiem.nhg.64cntt@ntu.edu.vn>
2025-03-13 15:31:30 +0700

    SB BMI

---
+++ SB_BMI/src/test/java/edu/khiemnhg/SB_BMI/SbBmiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.khiemnhg.SB_BMI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
