eb1cc657b10f0243cd0bc3c11cac29f33c39ff6d
KhiemCho <khiem.nhg.64cntt@ntu.edu.vn>
2025-03-25 09:25:09 +0700

    push lan 2

--- TongHopGK/src/main/java/edu/khiemnhg/TongHopGK/controllers/HomepageController.java
+++ TongHopGK/src/main/java/edu/khiemnhg/TongHopGK/controllers/HomepageController.java
@@ -1,13 +1,19 @@
 package edu.khiemnhg.TongHopGK.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.khiemnhg.TongHopGK.models.SinhVien;
 
 @Controller
 public class HomepageController {
 	@GetMapping("/")
-	public String homepage() {
+	public String homepage(ModelMap model) {
+		ArrayList<SinhVien> dssv = new ArrayList<SinhVien>();
+		model.addAttribute("dssv", dssv);
 		return "index";
 	}
 }
--- TongHopGK/src/main/java/edu/khiemnhg/TongHopGK/models/SinhVien.java
+++ TongHopGK/src/main/java/edu/khiemnhg/TongHopGK/models/SinhVien.java
@@ -1,5 +1,13 @@
 package edu.khiemnhg.TongHopGK.models;
 
 public class SinhVien {
-
+	String mssv;
+	String hoTen;
+	float diemTB;
+	public SinhVien(String mssv, String hoTen, float diemTB) {
+		// TODO Auto-generated constructor stub
+		this.mssv = mssv;
+		this.hoTen = hoTen;
+		this.diemTB = diemTB;
+	}
 }
