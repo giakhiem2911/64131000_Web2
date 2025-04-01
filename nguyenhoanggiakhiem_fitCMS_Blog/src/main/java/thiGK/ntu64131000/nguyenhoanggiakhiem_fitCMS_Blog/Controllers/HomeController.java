8499afce36907781d2af7a1883d84f33737b6510
KhiemCho <khiem.nhg.64cntt@ntu.edu.vn>
2025-04-01 09:24:10 +0700

    thi giua ky lan 2

--- nguyenhoanggiakhiem_fitCMS_Blog/src/main/java/thiGK/ntu64131000/nguyenhoanggiakhiem_fitCMS_Blog/Controllers/HomeController.java
+++ nguyenhoanggiakhiem_fitCMS_Blog/src/main/java/thiGK/ntu64131000/nguyenhoanggiakhiem_fitCMS_Blog/Controllers/HomeController.java
@@ -6,8 +6,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
-	public String homepage(ModelMap model) {
-		return "frontEndViews/index";
-	}
+    
+    @GetMapping("/")
+    public String homepage(ModelMap model) {
+        return "frontEndViews/index";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(ModelMap model) {
+        return "frontEndViews/dashboard";
+    }
+
+    @GetMapping("/page/list")
+    public String pageList(ModelMap model) {
+        return "frontEndViews/pageList";
+    }
+
+    @GetMapping("/page/addnew")
+    public String addNewPage(ModelMap model) {
+        return "frontEndViews/pageAddNew";
+    }
+
+    @GetMapping("/post/list")
+    public String postList(ModelMap model) {
+        return "frontEndViews/postList";
+    }
+
+    @GetMapping("/post/addnew")
+    public String addNewPost(ModelMap model) {
+        return "frontEndViews/postAddNew";
+    }
 }
